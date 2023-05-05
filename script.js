4381acb50664f20173360a3fc9bff10794847ceb
Adam <adamgjohnson92@gmail.com>
2023-05-04 18:02:40 -0700

    Added a for loop to the event listener to cardBackArray so the event listener is applied to all cardBack divs.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var cardEl = document.querySelectorAll(".card")
+var petMakiBtn = document.querySelector(".page-title")
+//var cardEl = document.querySelectorAll(".card")
+var cardBackArray = document.querySelectorAll(".face-down")
+
 var petMaki = function() {
     var petHer = window.confirm("Will you pet Maki and give her treats?")
     if (petHer) {
@@ -9,5 +12,18 @@ var petMaki = function() {
     return;
 }
 
-var petMakiBtn = document.querySelector(".page-title")
 petMakiBtn.addEventListener("click",petMaki);
+
+
+var cardFlip = function(event) {
+    var element = event.target;
+    if (element.matches(".face-down")) {
+        element.style.zIndex = "0"
+    }
+}
+
+for (let i = 0; i < cardBackArray.length; i++) {
+    console.log(cardBackArray[i]);
+    cardBackArray[i].addEventListener("click",cardFlip)
+}
+//console.log(cardBack)
