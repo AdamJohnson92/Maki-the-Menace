7790d87558db36c3d46d976aa703533f25380a08
Adam <adamgjohnson92@gmail.com>
2023-05-06 10:53:51 -0700

    Added new section for maki petting funcitonality. Removed event listener from the page title and added it to the image in the pet-maki section. Began framework for adding a counter for how many times a user has pet maki.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var petMakiBtn = document.querySelector(".page-title")
+var petMakiBtn = document.querySelector("#pet-maki-img")
 //var cardEl = document.querySelectorAll(".card")
 var cardBackArray = document.querySelectorAll(".face-down")
+var petCounter = document.querySelector("#counter")
 
 var petMaki = function() {
-    var petHer = window.confirm("Will you pet Maki and give her treats?")
+    var petHer = window.confirm("Will you pet Maki?")
     if (petHer) {
-        window.alert("Maki is pleased.")
+        window.alert("Maki is pleased.");
+
     } else {
         window.alert("Maki is displeased. You are in danger. Run.")
     }
@@ -18,7 +20,8 @@ petMakiBtn.addEventListener("click",petMaki);
 var cardFlip = function(event) {
     var element = event.target;
     if (element.matches(".face-down")) {
-        element.style.zIndex = "0"
+        element.style.zIndex = "0";
+        element.style.scale = "96%"
     }
 }
 
